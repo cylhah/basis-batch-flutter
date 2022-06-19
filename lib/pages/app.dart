61bbf6caef3066ad0a33dc591d7038b798dee589
cylhah <37016501+cylhah@users.noreply.github.com>
2022-06-19 16:26:25 +0800

    feat: 添加windowsize依赖

---
+++ lib/pages/app.dart
@@ -0,0 +1,16 @@
+import 'package:basis_batch/pages/home.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('111221'),
+      ),
+    );
+  }
+}
